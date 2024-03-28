64f698f5c89ea2d8bb205334395f8a6fd5b23b02
Eric Lin <tur99299@temple.edu>
2024-03-28 12:27:26 -0400

    Create viewPager2 in main layout, and create a value  viewPage. and then use its adapter to get fragmentstateAdapter, and then, get two function, getItem and createFragment and set getItem to return 10 fragments, set createFragment to return each fragment and set it to string.

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,15 +2,39 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
+    var numberOfFragments = 1
+
+    val newButton : Button by lazy {
+        findViewById(R.id.button)
+    }
+
+    private val viewPager: ViewPager2 by lazy {
+        findViewById(R.id.viewPager)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager.adapter = object: FragmentStateAdapter(this) {
+            override fun getItemCount() = numberOfFragments
+
+            override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
+
+        }
+
+        newButton.setOnClickListener{
+            numberOfFragments++
+            (viewPager.adapter as FragmentStateAdapter).notifyDataSetChanged()
+            viewPager.setCurrentItem(numberOfFragments -1)
+        }
 
     }
 
